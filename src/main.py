93cc9ebd89c1162165e6ddfa43ef85cd054f543c
AndreyVLD <nicula2003@gmail.com>
2025-05-11 20:18:10 +0200

    Added the correct dataset

--- src/main.py
+++ src/main.py
@@ -1,34 +1,40 @@
+from pathlib import Path
+
 import torch
 
 from transformers import AutoTokenizer
 from torch.optim import AdamW
 
-from pipeline.dataset import MockTestGenDataset
 from pipeline.model import StudentModel
 from pipeline.model import DistillationLoss
 from pipeline.train import train
+from pipeline.dataset import TestGenDataset
 
 
 def main() -> None:
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # Load the tokenizer
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #
+    # # Load the tokenizer
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-base")
-
-    # Load the dataset
-    dataset = MockTestGenDataset(tokenizer)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-
-    # Initialize the model
-    student = StudentModel(tokenizer).to(device)
-    student.config.use_cache = False
-
-    # Initialize the optimizer and loss function
-    criterion = DistillationLoss()
-    optimizer = AdamW(student.parameters(), lr=1e-4)
-
-    # Train the model
-    train(student, dataloader, optimizer, criterion, device, num_epochs=5)
+    #
+    # # Load the dataset
+    # dataset = MockTestGenDataset(tokenizer)
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+    #
+    # # Initialize the model
+    # student = StudentModel(tokenizer).to(device)
+    # student.config.use_cache = False
+    #
+    # # Initialize the optimizer and loss function
+    # criterion = DistillationLoss()
+    # optimizer = AdamW(student.parameters(), lr=1e-4)
+    #
+    # # Train the model
+    # train(student, dataloader, optimizer, criterion, device, num_epochs=5)
+    dataset = TestGenDataset(tokenizer)
+    sample = next(iter(dataset))
+    print(sample)
+    print(tokenizer.decode(sample['input_ids'], skip_special_tokens=True))
 
 
 if __name__ == '__main__':
--- src/pipeline/dataset.py
+++ src/pipeline/dataset.py
@@ -1,50 +1,66 @@
 import json
-from pathlib import Path
-
 import torch
-from torch.utils.data import Dataset
+from pathlib import Path
 from transformers import T5Tokenizer
+from typing import Optional, Any, TypedDict, Iterator
+from torch.utils.data import IterableDataset
+
+from utils.decompression import decompress_tensor_optimized
 
 
-class MockTestGenDataset(Dataset):
+class Sample(TypedDict):
+    input_ids: torch.Tensor
+    attention_mask: torch.Tensor
+    labels: torch.Tensor
+    teacher_logits: Optional[torch.Tensor]
+
+
+class RawEntry(TypedDict):
+    repository: str
+    focal_file: str
+    test_method_masked: str
+    assertions: list[str]
+    method_under_test: str
+    teacher_prediction: str
+    teacher_parsed_assertions: list[str]
+    teacher_metrics: dict[str, Any]
+    teacher_logits: Any
+
+
+class TestGenDataset(IterableDataset):
     """
-    A mock dataset that uses hard-coded method-test pairs.
-    Tokenizes inputs/tests and creates random teacher logits.
+    Dataset for assert generation.
+    This dataset is used to load and preprocess the data for training and evaluation.
     """
 
     def __init__(self, tokenizer: T5Tokenizer, max_len=64):
-        data_path = Path(__file__).resolve().parents[2] / "data" / "sample_data.json"
-        with data_path.open("r") as f:
-            self.samples = json.load(f)
+        self.file_path = Path(__file__).resolve().parents[2] / "data" / "dataset_with_predictions.jsonl"
 
         self.tokenizer = tokenizer
         self.max_len = max_len
-        self.vocab_size = 32128
 
-    def __len__(self):
-        return len(self.samples)
+    def __iter__(self) -> Iterator[Sample]:
+        with open(self.file_path, 'r') as f:
+            for line in f:
+                raw: RawEntry = json.loads(line)
 
-    def __getitem__(self, idx: int):
-        sample = self.samples[idx]
-        src = sample['method']
-        trg = sample['test']
+                # TODO: Extend to increase the context length
+                src = raw['test_method_masked']
+                trg = raw['teacher_prediction']
 
-        # Tokenize source and target together for encoder-decoder
-        enc = self.tokenizer(src, padding='max_length', truncation=True, max_length=self.max_len, return_tensors='pt')
-        dec = self.tokenizer(trg, padding='max_length', truncation=True, max_length=self.max_len, return_tensors='pt')
+                enc = self.tokenizer(src, padding='max_length', truncation=True, max_length=self.max_len,
+                                     return_tensors='pt')
+                dec = self.tokenizer(trg, padding='max_length', truncation=True, max_length=self.max_len,
+                                     return_tensors='pt')
 
-        input_ids = enc.input_ids.squeeze(0)
-        attention_mask = enc.attention_mask.squeeze(0)
-        labels = dec.input_ids.squeeze(0)
+                sample: Sample = {
+                    'input_ids': enc.input_ids.squeeze(0),
+                    'attention_mask': enc.attention_mask.squeeze(0),
+                    'labels': dec.input_ids.squeeze(0),
+                    'teacher_logits': None
+                }
 
-        # Create random teacher logits matching [seq_len, vocab_size]
-        # TODO: Replace with actual teacher model logits
-        seq_len = labels.size(0)
-        teacher_logits = torch.randn(seq_len, self.vocab_size)
+                if raw.get('teacher_logits') is not None:
+                    sample['teacher_logits'] = decompress_tensor_optimized(raw['teacher_logits'])
 
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attention_mask,
-            'labels': labels,
-            'teacher_logits': teacher_logits
-        }
+                yield sample
